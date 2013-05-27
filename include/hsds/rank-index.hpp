14de450673cb6c981d604e34dee8f9eb2ebb7c4b
Hideaki Ohno <hide.o.j55@gmail.com>
2013-05-27 19:21:54 +0900

    Update rank-index.hpp

--- include/hsds/rank-index.hpp
+++ include/hsds/rank-index.hpp
@@ -18,31 +18,31 @@ public:
     }
 
     void set_rel1(uint64_t value) {
-        rel_ = ((rel_ & ~0x7FLLU) | (value & 0x7FLLU));
+        rel_ = ((rel_ & ~0x7FULL) | (value & 0x7FULL));
     }
 
     void set_rel2(uint64_t value) {
-        rel_ = ((rel_ & ~(0xFFLLU << 7)) | (value & 0xFFLLU) << 7);
+        rel_ = ((rel_ & ~(0xFFULL << 7)) | (value & 0xFFULL) << 7);
     }
 
     void set_rel3(uint64_t value) {
-        rel_ = ((rel_ & ~(0xFFLLU << 15)) | (value & 0xFFLLU) << 15);
+        rel_ = ((rel_ & ~(0xFFULL << 15)) | (value & 0xFFULL) << 15);
     }
 
     void set_rel4(uint64_t value) {
-        rel_ = ((rel_ & ~(0x1FFLLU << 23)) | (value & 0x1FFLLU) << 23);
+        rel_ = ((rel_ & ~(0x1FFULL << 23)) | (value & 0x1FFULL) << 23);
     }
 
     void set_rel5(uint64_t value) {
-        rel_ = ((rel_ & ~(0x1FFLLU << 32)) | (value & 0x1FFLLU) << 32);
+        rel_ = ((rel_ & ~(0x1FFULL << 32)) | (value & 0x1FFULL) << 32);
     }
 
     void set_rel6(uint64_t value) {
-        rel_ = ((rel_ & ~(0x1FFLLU << 41)) | (value & 0x1FFLLU) << 41);
+        rel_ = ((rel_ & ~(0x1FFULL << 41)) | (value & 0x1FFULL) << 41);
     }
 
     void set_rel7(uint64_t value) {
-        rel_ = ((rel_ & ~(0x1FFLLU << 50)) | (value & 0x1FFLLU) << 50);
+        rel_ = ((rel_ & ~(0x1FFULL << 50)) | (value & 0x1FFULL) << 50);
     }
 
     uint64_t abs() const {
@@ -50,31 +50,31 @@ public:
     }
 
     uint64_t rel1() const {
-        return rel_ & 0x7FLLU;
+        return rel_ & 0x7FULL;
     }
 
     uint64_t rel2() const {
-        return (rel_ >> 7) & 0xFFLLU;
+        return (rel_ >> 7) & 0xFFULL;
     }
 
     uint64_t rel3() const {
-        return (rel_ >> 15) & 0xFFLLU;
+        return (rel_ >> 15) & 0xFFULL;
     }
 
     uint64_t rel4() const {
-        return (rel_ >> 23) & 0x1FFLLU;
+        return (rel_ >> 23) & 0x1FFULL;
     }
 
     uint64_t rel5() const {
-        return (rel_ >> 32) & 0x1FFLLU;
+        return (rel_ >> 32) & 0x1FFULL;
     }
 
     uint64_t rel6() const {
-        return (rel_ >> 41) & 0x1FFLLU;
+        return (rel_ >> 41) & 0x1FFULL;
     }
 
     uint64_t rel7() const {
-        return (rel_ >> 50) & 0x1FFLLU;
+        return (rel_ >> 50) & 0x1FFULL;
     }
 
 private:
