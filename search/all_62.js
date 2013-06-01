d5a5e62b6f8f5d92f76f7b1048ba64b595d35572
hideo55 <hide.o.j55@gmail.com>
2013-06-01 19:25:00 +0900

    Update document

--- search/all_62.js
+++ search/all_62.js
@@ -4,5 +4,5 @@ var searchData=
   ['bitvector',['BitVector',['../classhsds_1_1_bit_vector.html',1,'hsds']]],
   ['bitvector',['BitVector',['../classhsds_1_1_bit_vector.html#ab524f702348c9b9adeaac41bcbd7c74e',1,'hsds::BitVector::BitVector()'],['../classhsds_1_1_bit_vector.html#a7a2b43bfdfa0e713c4a4904e30aef615',1,'hsds::BitVector::BitVector(uint64_t size)']]],
   ['block_5frate',['BLOCK_RATE',['../namespacehsds.html#a7fbdb97fb7f9e02b4ba7deeaca45045b',1,'hsds']]],
-  ['build',['build',['../classhsds_1_1_bit_vector.html#a6fc969a0781bbbfb842ad1ef2278cbeb',1,'hsds::BitVector']]]
+  ['build',['build',['../classhsds_1_1_bit_vector.html#a928a0540638b3e1a6f69cb3c27038a7f',1,'hsds::BitVector']]]
 ];
