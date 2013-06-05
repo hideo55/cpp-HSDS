8eabec9d441c77d1538125a730e220ee3b613585
hideo55 <hide.o.j55@gmail.com>
2013-06-06 02:28:05 +0900

    Update docs

--- search/all_72.js
+++ search/all_72.js
@@ -2,13 +2,14 @@ var searchData=
 [
   ['rank',['rank',['../classhsds_1_1_bit_vector.html#ab522c5a9364e1679fc5a895e45141b8f',1,'hsds::BitVector']]],
   ['rank_2dindex_2ehpp',['rank-index.hpp',['../rank-index_8hpp.html',1,'']]],
-  ['rankindex',['RankIndex',['../classhsds_1_1_rank_index.html',1,'hsds']]],
   ['rankindex',['RankIndex',['../classhsds_1_1_rank_index.html#aafd04d0aff74bdfa1b3a0a7972ac0cda',1,'hsds::RankIndex']]],
+  ['rankindex',['RankIndex',['../classhsds_1_1_rank_index.html',1,'hsds']]],
   ['rel1',['rel1',['../classhsds_1_1_rank_index.html#a31876486ce61ad1730240e24b802871c',1,'hsds::RankIndex']]],
   ['rel2',['rel2',['../classhsds_1_1_rank_index.html#a1e2e688277ec6d3ecb9ab525a9aeffd1',1,'hsds::RankIndex']]],
   ['rel3',['rel3',['../classhsds_1_1_rank_index.html#aa1f570c9716211e87748bdc3709891bd',1,'hsds::RankIndex']]],
   ['rel4',['rel4',['../classhsds_1_1_rank_index.html#abca3ce6d3bde5b388dd5049f0028eb44',1,'hsds::RankIndex']]],
   ['rel5',['rel5',['../classhsds_1_1_rank_index.html#a766c0225af29096f0473fef3c46fc66e',1,'hsds::RankIndex']]],
   ['rel6',['rel6',['../classhsds_1_1_rank_index.html#a53fe143d47c57240b4db87faf407e784',1,'hsds::RankIndex']]],
-  ['rel7',['rel7',['../classhsds_1_1_rank_index.html#aedcab9aaf745a9af6b932d7bc680a0db',1,'hsds::RankIndex']]]
+  ['rel7',['rel7',['../classhsds_1_1_rank_index.html#aedcab9aaf745a9af6b932d7bc680a0db',1,'hsds::RankIndex']]],
+  ['rnakindex',['RnakIndex',['../class_rnak_index.html',1,'']]]
 ];
--- search/classes_72.js
+++ search/classes_72.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['rankindex',['RankIndex',['../classhsds_1_1_rank_index.html',1,'hsds']]]
+  ['rankindex',['RankIndex',['../classhsds_1_1_rank_index.html',1,'hsds']]],
+  ['rnakindex',['RnakIndex',['../class_rnak_index.html',1,'']]]
 ];
