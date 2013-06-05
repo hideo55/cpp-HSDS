a8ccf8972ae93aa481ed12d602af75e336e66788
hideo55 <hide.o.j55@gmail.com>
2013-06-06 02:56:38 +0900

    Update docs

--- search/classes_72.js
+++ search/classes_72.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['rankindex',['RankIndex',['../classhsds_1_1_rank_index.html',1,'hsds']]],
-  ['rnakindex',['RnakIndex',['../class_rnak_index.html',1,'']]]
+  ['rankindex',['RankIndex',['../classhsds_1_1_rank_index.html',1,'hsds']]]
 ];
