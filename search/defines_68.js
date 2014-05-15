fac4628bdb981219957200b06ff7da7c49cd2aed
hideo55 <hide.o.j55@gmail.com>
2014-05-16 02:34:31 +0900

    Update docs

--- search/defines_68.js
+++ search/defines_68.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['hsds_5fdebug_5fif',['HSDS_DEBUG_IF',['../exception_8hpp.html#accb4b931ee78dfcd134af6f7f5f7fc5b',1,'exception.hpp']]],
   ['hsds_5fexception_5fif',['HSDS_EXCEPTION_IF',['../exception_8hpp.html#a7609ec4714643a95cacc5da65456a6be',1,'exception.hpp']]]
 ];
