9bc09e5be8b530b43fe36cbc891627d3b200a971
Hideaki Ohno <hide.o.j55@gmail.com>
2014-05-29 09:50:03 +0900

    Update test_wavelet-matrix.cpp

--- t/test_wavelet-matrix.cpp
+++ t/test_wavelet-matrix.cpp
@@ -13,23 +13,26 @@ using namespace hsds;
 #define AssertThatEx(X,Y) Assert::That(X, Y, __FILE__, __LINE__)
 
 Describe(wavelet_matrix) {
-    It(create_instance) {
+    It(T001_create_instance) {
         WaveletMatrix* wm = new WaveletMatrix();
         AssertThatEx(wm != NULL, Is().EqualTo(true));
         AssertThatEx(wm->size(), Is().EqualTo(0));
         delete wm;
     }
 
-    It(build) {
+    It(T002_build_and_lookup) {
         WaveletMatrix wm;
         vector<uint64_t> v;
         v.push_back(5);
-        v.push_back(0);
-        v.push_back(10);
-        v.push_back(1);
+        v.push_back(4);
         v.push_back(3);
+        v.push_back(0);
+        v.push_back(0);
         wm.build(v);
-        cout << wm.lookup(0) << endl;
+
+        for(size_t i = 0; i < v.size(); ++i){
+            AssertThatEx(wm.lookup(i), Is().EqualTo(v[i]));
+        }
     }
 };
 
