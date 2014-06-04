2555ecc22d14798552aa510c42527c68e4929c37
hideo55 <hide.o.j55@gmail.com>
2014-06-04 20:48:55 +0900

    Update docs

--- search/functions_6f.js
+++ search/functions_6f.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['operator_3c_3c',['operator&lt;&lt;',['../namespacehsds.html#a463e5fb8b3fcf96d5cbac00d7e81567f',1,'hsds']]],
-  ['operator_5b_5d',['operator[]',['../classhsds_1_1_bit_vector.html#a8d42a83bae6c46fe5aedd8a0fe3b00ee',1,'hsds::BitVector']]]
+  ['operator_5b_5d',['operator[]',['../classhsds_1_1_bit_vector.html#a8d42a83bae6c46fe5aedd8a0fe3b00ee',1,'hsds::BitVector::operator[]()'],['../classhsds_1_1_wavelet_matrix.html#a9f5acae7818ebfeccabc9c0b235a60df',1,'hsds::WaveletMatrix::operator[]()']]]
 ];
