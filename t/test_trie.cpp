f7ac132b368504e3e6f1b93e4b49588d3b95d162
hideo55 <hide.o.j55@gmail.com>
2014-07-14 02:28:40 +0900

    Update test

--- t/test_trie.cpp
+++ t/test_trie.cpp
@@ -16,45 +16,79 @@ using namespace hsds;
 
 #define AssertThatEx(X,Y) Assert::That(X, Y, __FILE__, __LINE__)
 
-
 Describe(hsds_Trie) {
-    It(create_instance) {
+    It(t01_create_instance) {
         Trie* trie = new Trie();
         AssertThatEx(trie != NULL, Is().EqualTo(true));
         delete trie;
     }
 
-    Describe(build_trie) {
-        It(build_normal_trie) {
-            std::vector<string> keyList;
-            keyList.push_back("bbc");
-            keyList.push_back("able");
-            keyList.push_back("abc");
-            keyList.push_back("abcde");
-            keyList.push_back("can");
+    It(t02_build_normal_trie) {
+        std::vector<string> keyList;
+        keyList.push_back("bbc");
+        keyList.push_back("able");
+        keyList.push_back("abc");
+        keyList.push_back("abcde");
+        keyList.push_back("can");
 
-            Trie trie;
-            trie.build(keyList);
+        Trie trie;
+        trie.build(keyList);
 
-            vector<string>::const_iterator iter = keyList.begin();
-            vector<string>::const_iterator iter_end = keyList.end();
-            for(; iter != iter_end; ++iter) {
-                Trie::id_t id = trie.exactMatchSearch(iter->c_str(), iter->size());
-                AssertThatEx(id, IsLessThan(keyList.size()));
-                string ret;
-                trie.decodeKey(id, ret);
-                AssertThatEx(ret.c_str(), Equals(iter->c_str()));
-            }
-
-            vector<Trie::id_t> ids;
-            trie.commonPrefixSearch("abcde", 5, ids);
-            AssertThatEx(ids.size(), Equals(2));
-
-            ids.clear();
-            trie.predictiveSearch("ab", 2, ids);
-            AssertThatEx(ids.size(), Equals(3));
+        vector<string>::const_iterator iter = keyList.begin();
+        vector<string>::const_iterator iter_end = keyList.end();
+        for (; iter != iter_end; ++iter) {
+            Trie::id_t id = trie.exactMatchSearch(iter->c_str(), iter->size());
+            AssertThatEx(id, IsLessThan(keyList.size()));
+            string ret;
+            trie.decodeKey(id, ret);
+            AssertThatEx(ret.c_str(), Equals(iter->c_str()));
         }
-    };
+
+        vector<Trie::id_t> ids;
+        trie.commonPrefixSearch("abcde", 5, ids);
+        AssertThatEx(ids.size(), Equals(2));
+
+        ids.clear();
+        trie.predictiveSearch("ab", 2, ids);
+        AssertThatEx(ids.size(), Equals(3));
+    }
+
+    It(t03_commomPrefixSearch) {
+        std::vector<string> keyList;
+        keyList.push_back("bbc");
+        keyList.push_back("able");
+        keyList.push_back("abc");
+        keyList.push_back("abcde");
+        keyList.push_back("can");
+
+        Trie trie;
+        trie.build(keyList);
+
+        vector<Trie::id_t> ids;
+        trie.commonPrefixSearch("abcde", 5, ids);
+        AssertThatEx(ids.size(), Equals(2));
+        string ret;
+        trie.decodeKey(ids[0], ret);
+        AssertThatEx(ret, Equals(string("abc")));
+        trie.decodeKey(ids[1], ret);
+        AssertThatEx(ret, Equals(string("abcde")));
+
+        vector<Trie::Result> results;
+        trie.commonPrefixSearch("abcde", 5, results);
+        AssertThatEx(results.size(), Equals(2));
+
+        trie.decodeKey(results[0].id, ret);
+        AssertThatEx(ret, Equals(string("abc")));
+        AssertThatEx(results[0].depth, Equals(3));
+        trie.decodeKey(results[1].id, ret);
+        AssertThatEx(ret, Equals(string("abcde")));
+        AssertThatEx(results[1].depth, Equals(5));
+    }
+
+    It(t04_predictiveSearch){
+
+    }
+
 };
 
 int main() {
