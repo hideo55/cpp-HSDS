d8fa08f6b1af1c21db3e3c99849990b450884ed7
hideo55 <hide.o.j55@gmail.com>
2014-07-22 01:44:49 +0900

    Update docs

--- search/functions_73.js
+++ search/functions_73.js
@@ -6,6 +6,6 @@ var searchData=
   ['select1',['select1',['../classhsds_1_1_bit_vector.html#a4020dd48178e0a01c85883abe0817c47',1,'hsds::BitVector']]],
   ['selectfrompos',['selectFromPos',['../classhsds_1_1_wavelet_matrix.html#a955adb66a706d07594958ff1452433e0',1,'hsds::WaveletMatrix']]],
   ['set',['set',['../classhsds_1_1_bit_vector.html#a41426138566c030a1d88cfb17812c392',1,'hsds::BitVector']]],
-  ['size',['size',['../classhsds_1_1_bit_vector.html#a341034a47b57b028abeef4070bf16a25',1,'hsds::BitVector::size() const '],['../classhsds_1_1_bit_vector.html#a03819da082338d69fd97c10640b0b642',1,'hsds::BitVector::size(bool b) const '],['../classhsds_1_1_wavelet_matrix.html#aa800377b4a4fcf66936db405a0c5db6b',1,'hsds::WaveletMatrix::size()']]],
+  ['size',['size',['../classhsds_1_1_bit_vector.html#a341034a47b57b028abeef4070bf16a25',1,'hsds::BitVector::size() const '],['../classhsds_1_1_bit_vector.html#a03819da082338d69fd97c10640b0b642',1,'hsds::BitVector::size(bool b) const '],['../classhsds_1_1_trie.html#a9265d79654d1af7ab54b6da7e2d0f524',1,'hsds::Trie::size()'],['../classhsds_1_1_wavelet_matrix.html#aa800377b4a4fcf66936db405a0c5db6b',1,'hsds::WaveletMatrix::size()']]],
   ['swap',['swap',['../classhsds_1_1_bit_vector.html#a43fb6533bdb1ae8cdf070d686f90bfe6',1,'hsds::BitVector::swap()'],['../classhsds_1_1_trie.html#a6ab1314b96ab8c082aee37641ee57853',1,'hsds::Trie::swap()'],['../classhsds_1_1_wavelet_matrix.html#a105859b59580d7708ed7e4905a83751e',1,'hsds::WaveletMatrix::swap()']]]
 ];
