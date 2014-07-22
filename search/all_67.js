2b2b9022be35b8d4bdcfbedda68ae778a4fabce2
hideo55 <hide.o.j55@gmail.com>
2014-07-23 01:41:20 +0900

    Update docs

--- search/all_67.js
+++ search/all_67.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['get_5fbits',['get_bits',['../classhsds_1_1_bit_vector.html#ac93ff67e3ec03567d3541b35b1480d3b',1,'hsds::BitVector']]],
   ['getfilename',['getFileName',['../classhsds_1_1_exception.html#a0bdea84b29334e7ab2ce5cfdb4896b80',1,'hsds::Exception']]],
   ['getfunctionname',['getFunctionName',['../classhsds_1_1_exception.html#a46cf696bf11334d0fdd6a90dc8332ce4',1,'hsds::Exception']]],
   ['getlinenumber',['getLineNumber',['../classhsds_1_1_exception.html#a1d0f68356dd59a82bd74a70a3df01bed',1,'hsds::Exception']]]
--- search/all_70.js
+++ search/all_70.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['predictivesearch',['predictiveSearch',['../classhsds_1_1_trie.html#a85b21a956a80e1a489b02414cdfff199',1,'hsds::Trie']]]
+  ['predictivesearch',['predictiveSearch',['../classhsds_1_1_trie.html#a85b21a956a80e1a489b02414cdfff199',1,'hsds::Trie']]],
+  ['push_5fback_5fbits',['push_back_bits',['../classhsds_1_1_bit_vector.html#af357f43bb45b7095dcee661728d06de2',1,'hsds::BitVector']]]
 ];
