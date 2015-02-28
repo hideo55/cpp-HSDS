c5009a51fe3676d3c09379525ad3a710010ef7e4
hideo55 <hide.o.j55@gmail.com>
2015-02-28 20:48:46 +0900

    Update docs

--- search/all_70.js
+++ search/all_70.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['predictivesearch',['predictiveSearch',['../classhsds_1_1_trie.html#a85b21a956a80e1a489b02414cdfff199',1,'hsds::Trie']]],
+  ['push_5fback',['push_back',['../classhsds_1_1_bit_vector.html#a39a6d6d1da5b58385c9a3ee7462ce2a4',1,'hsds::BitVector']]],
   ['push_5fback_5fbits',['push_back_bits',['../classhsds_1_1_bit_vector.html#af357f43bb45b7095dcee661728d06de2',1,'hsds::BitVector']]]
 ];
